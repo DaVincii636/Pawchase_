e833bc0324e4f2688710b3d6b40908e2cd57b09f
YamBenedict <yambenedict02@gmail.com>
2026-04-20 08:44:13 +0800

    - Added the Startup.cs, to be used for the Google Login Auth - Added and Installed Microsoft Owin (includes the Security and System Web) to be used as bridge for Google Authentication. - In Web.config, added a key to temporarily disable the function of Owin to focus on building the SQL Schema for Google Logs, that includes the sign in and sign outs.

---
+++ Pawchase/App_Start/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+
+namespace Pawchase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 1. Setup Cookie Authentication
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+
+            // 2. Allow the app to temporarily store user info during the login process
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // 3. Configure Google Authentication
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "YOUR_CLIENT_ID.apps.googleusercontent.com",
+                ClientSecret = "YOUR_CLIENT_SECRET"
+            });
+        }
+    }
+}
+
