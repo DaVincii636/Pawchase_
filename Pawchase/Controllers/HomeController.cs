52d980d68ac16e46607c195451169edd1e0c7ebf
UltimateFury26 <guadesmarkrafaellim@gmail.com>
2026-05-13 15:59:30 +0800

    test

--- Pawchase/Controllers/HomeController.cs
+++ Pawchase/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace Pawchase.Controllers
             return View();
         }
 
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
         // ── Global error pages ────────────────────────────────────────
         public ActionResult Error()
         {
